78af8ffe17adedeee4209927c779460a6a1eaf7a
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2023-07-21 16:08:04 +0300

    making rivers layouts

--- app/src/main/java/com/example/pikelindner/OligoLakeDescription.kt
+++ app/src/main/java/com/example/pikelindner/OligoLakeDescription.kt
@@ -1,4 +1,30 @@
 package com.example.pikelindner
 
-class OligoLakeDescription {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.instruction.*
+import kotlinx.android.synthetic.main.oligo_lake_description.*
+//todo сделать после активити категории озёр
+
+class OligoLakeDescription: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.oligo_lake_description)
+        supportActionBar?.hide()
+
+        buttonBack.setOnClickListener{
+            val intent = Intent(this, Lakes::class.java)
+            startActivity(intent)
+        }
+        buttonNext.setOnClickListener{
+            val intent = Intent(this, Lakes::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
 }
--- app/src/main/java/com/example/pikelindner/PondsCategories.kt
+++ app/src/main/java/com/example/pikelindner/PondsCategories.kt
@@ -1,11 +1,14 @@
 package com.example.pikelindner
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.ponds_categories.*
 
 
 class PondsCategories : AppCompatActivity() {
@@ -15,5 +18,21 @@ class PondsCategories : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ponds_categories)
         supportActionBar?.hide()
+
+        buttonLakes.setOnClickListener{
+            val intent = Intent(this, Lakes::class.java)
+            startActivity(intent)
+        }
+
+        buttonRivers.setOnClickListener{
+            val intent = Intent(this, Instruction::class.java)
+            startActivity(intent)
+        }
+
+        buttonStoragePond.setOnClickListener{
+            val intent = Intent(this, Instruction::class.java)
+            startActivity(intent)
+        }
+
     }
 }
